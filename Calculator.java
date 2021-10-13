3a09b38b0027e2db64e389a986a3b949f2953aae
Bruno <braga.bruno81@gmail.com>
2021-10-13 17:50:59 -0500

    my fswitch statment

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator{
+
+	public static void main(String[] args){
+		weekday("monday");	
+	}
+	
+	public int add(int a , int b){
+		return a + b;
+	}
+	
+	public int sum(int a, int b){
+		return a - b;
+	}
+	
+	public static double multiply(double a, double b){
+		return a * b;
+	}
+	public static int remainder(int a, int b){
+		return a % b;
+	}
+
+	public static void weekday(String weekday){
+		switch(weekday){
+			case "monday":
+				System.out.println("Today is monday");
+				break;
+			case "tuesday":
+				System.out.println("Today is Tuesday");
+				break;
+			default: 
+		}
+	}
+}
