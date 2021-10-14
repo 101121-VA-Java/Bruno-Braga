1bd44c099cac8ede237e161d102a9201a0751ce4
Bruno <braga.bruno81@gmail.com>
2021-10-14 14:58:15 -0500

    control flow statement method

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,40 @@
-public class Calculator{
+public class Calculator {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		weekday("monday");
 		String s = "tuesday";
-		weekday(args[0]);	
+		weekday(args[0]);
 		weekday(args[1]);
 		weekday(args[2]);
+
 	}
-	
-	public int add(int a , int b){
+
+	public static int add(int a, int b) {
 		return a + b;
 	}
-	
-	public int sum(int a, int b){
+
+	public int sum(int a, int b) {
 		return a - b;
 	}
-	
-	public static double multiply(double a, double b){
+
+	public static double multiply(double a, double b) {
 		return a * b;
 	}
-	public static int remainder(int a, int b){
+
+	public static int remainder(int a, int b) {
 		return a % b;
 	}
 
-	public static void weekday(String weekday){
-		switch(weekday){
+	public static void weekday(String weekday) {
+		switch (weekday) {
 			case "monday":
 				System.out.println("Today is monday");
 				break;
 			case "tuesday":
 				System.out.println("Today is Tuesday");
 				break;
-			default: 
+			default:
 		}
-		
 
 	}
 }
