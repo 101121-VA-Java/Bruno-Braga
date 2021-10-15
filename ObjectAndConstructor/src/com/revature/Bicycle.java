79d7716691ff53d8db60a3aebbd56d26406d96bc
Bruno <braga.bruno81@gmail.com>
2021-10-15 01:06:49 -0500

    my first java constructor

---
+++ ObjectAndConstructor/src/com/revature/Bicycle.java
@@ -0,0 +1,10 @@
+package com.revature;
+public class Bicycle {
+
+	public String model;
+	
+	public Bicycle(String model) {
+        this.model = model;
+    }
+	
+}
---
+++ ObjectAndConsturctor/src/com/revature/MyBikes.java
@@ -0,0 +1,9 @@
+package com.revature;
+
+public class MyBikes {
+
+	public static void main(String[] args) {
+		Bicycle mountainBike = new Bicycle("Ibis Ripmo V2 XT");
+		System.out.println(mountainBike.model);
+	}
+}
