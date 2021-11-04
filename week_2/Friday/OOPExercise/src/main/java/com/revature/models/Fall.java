18dd1133988e4f67cc5a10766f3375bf2ec2e542
bragabruno <bragabruno@users.noreply.github.com>
2021-11-04 08:26:51 -0500

    Delete week_2 directory

--- week_2/Friday/OOPExercise/src/main/java/com/revature/models/Fall.java
+++
@@ -1,8 +0,0 @@
-package com.revature.models;
-
-public class Fall extends Season{
-
-    public Fall(String seasonName) {
-        
-    }
-}
--- week_2/Friday/OOPExercise/src/main/java/com/revature/models/MildWeather.java
+++
@@ -1,8 +0,0 @@
-package com.revature.models;
-
-public class MildWeather {
-
-    void mildWeather() { // example of a method
-        System.out.println("It's cold and sunny");
-    }
-}
